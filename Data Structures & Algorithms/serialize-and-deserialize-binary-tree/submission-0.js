6615d2305ee0e5e26f7ac21a9cbbfe83ba57f845
mzvast <mzvast@gmail.com>
2026-09-11 15:43:47 +0800

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.js
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Codec {
+    /**
+     * Encodes a tree to a single string.
+     *
+     * @param {TreeNode} root
+     * @return {string}
+     */
+    serialize(root) {
+        // preorder
+
+        let ans = []; // null as #
+
+        function dfs(node) {
+            if (!node) {
+                ans.push("#");
+                return;
+            }
+            ans.push(node.val);
+            dfs(node.left);
+            dfs(node.right);
+        }
+
+        dfs(root);
+
+        return ans.join(",");
+    }
+
+    /**
+     * Decodes your encoded data to tree.
+     *
+     * @param {string} data
+     * @return {TreeNode}
+     */
+    deserialize(data) {
+        if (data.length === 0) return null;
+
+        const list = data.split(",");
+
+        let idx = 0;
+
+        function dfs() {
+            const value = list[idx++];
+            if (value === "#") return null;
+
+            const node = new TreeNode(+value);
+
+            node.left = dfs();
+            node.right = dfs();
+            return node;
+        }
+
+        return dfs();
+
+    }
+}
