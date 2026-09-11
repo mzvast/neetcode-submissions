4765e69def2d4856af1b11bf84fa9588d64e4fe4
mzvast <mzvast@gmail.com>
2026-09-11 14:42:52 +0800

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number[]} preorder
+     * @param {number[]} inorder
+     * @return {TreeNode}
+     */
+    buildTree(preorder, inorder) {
+        // if (preorder.length === 0) return null;
+        // // if (preorder.length === 1) return new TreeNode(preorder[0]);
+        // const root = preorder[0];
+        // // slice := [a,b)
+        // const idxOfRootInorder = inorder.indexOf(root);
+        // const leftInorder = inorder.slice(0, idxOfRootInorder),
+        //     rightInorder = inorder.slice(idxOfRootInorder + 1);
+
+        // const leftPreorder = preorder.slice(1, 1 + leftInorder.length),
+        //     rightPreorder = preorder.slice(1 + leftInorder.length);
+
+        // const node = new TreeNode(root);
+        // node.left = this.buildTree(leftPreorder, leftInorder);
+        // node.right = this.buildTree(rightPreorder, rightInorder);
+        // return node;
+
+        const positions = new Map(inorder.map((v, i) => [v, i]));
+
+        function build(preStart, inStart, size) {
+            if (size === 0) return null;
+
+            const rootVal = preorder[preStart];
+            const rootIdxIn = positions.get(rootVal);
+
+            const leftSize = rootIdxIn - inStart;
+            const rightSize = size - leftSize - 1;
+
+            const node = new TreeNode(rootVal);
+            node.left = build(preStart + 1, inStart, leftSize);
+            node.right = build(preStart + 1 + leftSize, rootIdxIn + 1, rightSize);
+            return node;
+        }
+
+        return build(0, 0, preorder.length);
+    }
+}
