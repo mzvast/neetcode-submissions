06d0a6e62bdbae81bc6826b915fe15c0b03492fd
mzvast <mzvast@gmail.com>
2026-09-14 14:52:14 +0800

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        nums.sort((a, b) => a - b);
+
+        const ans = [];
+        function bt(idx, path, target) {
+            if (target === 0) {
+                ans.push(path.slice());
+                return;
+            }
+            if (target < 0) {
+                return;
+            }
+
+            for (let i = idx; i < nums.length; i++) {
+                path.push(nums[i]);
+                bt(i, path, target - nums[i]);
+                path.pop();
+            }
+        }
+
+        bt(0, [], target);
+        return ans;
+    }
+}
