c808c151b13b8ea3eea106d3764e8ec8670a65db
mzvast <mzvast@gmail.com>
2026-09-14 12:02:45 +0800

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.js
@@ -0,0 +1,76 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    findWords(board, words) {
+        const m = board.length,
+            n = board[0].length;
+
+        const visited = Array.from({ length: m }, () => Array(n).fill(false));
+
+        class Node {
+            constructor() {
+                this.next = {};
+                this.word = null;
+            }
+        }
+
+        class Trie {
+            constructor() {
+                this.root = new Node();
+            }
+
+            addWord(word) {
+                let cur = this.root;
+                for (let x of word) {
+                    if (!cur.next[x]) cur.next[x] = new Node();
+                    cur = cur.next[x];
+                }
+                cur.word = word;
+            }
+        }
+
+        const t = new Trie();
+
+        for (let word of words) {
+            t.addWord(word);
+        }
+
+        // main search logic
+
+        const ans = [];
+
+        function dfs(i, j, node) {
+            if (!node) return;
+
+            if (i < 0 || i >= m || j < 0 || j >= n || !node.next[board[i][j]] || visited[i][j]) {
+                return;
+            }
+            visited[i][j] = true;
+
+            const nextNode = node.next[board[i][j]];
+
+            if(nextNode.word){
+                ans.push(nextNode.word)
+                nextNode.word = null // dedupe
+            }
+
+            dfs(i + 1, j, nextNode);
+            dfs(i - 1, j, nextNode);
+            dfs(i, j + 1, nextNode);
+            dfs(i, j - 1, nextNode);
+
+            visited[i][j] = false;
+        }
+
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                dfs(i, j, t.root);
+            }
+        }
+
+        return ans;
+    }
+}
