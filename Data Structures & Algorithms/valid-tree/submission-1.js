5f8861353cff216dde06247bcda8babfea5b7093
mzvast <mzvast@gmail.com>
2026-09-14 15:55:40 +0800

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        // toposort
+
+        if (edges.length !== n - 1) return false;
+
+        const g = Array.from({ length: n }, () => []); // to other point
+
+        for (let [from, to] of edges) {
+            g[from].push(to);
+            g[to].push(from);
+        }
+
+        let sorted = 0;
+        const visited = Array(n).fill(false);
+
+        const q = [0];
+
+        while (q.length) {
+            const cur = q.shift();
+
+            if (!visited[cur]) {
+                visited[cur] = true;
+                sorted += 1;
+            } else {
+                continue;
+            }
+
+            for (let to of g[cur]) {
+                if (visited[to]) continue;
+                q.push(to);
+            }
+        }
+
+        return sorted === n;
+    }
+}
