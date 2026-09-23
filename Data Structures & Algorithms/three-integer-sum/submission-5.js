438f44790681ee60c56475d0bac1303c3fe07aef
mzvast <mzvast@gmail.com>
2026-09-23 17:53:01 +0800

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const n = nums.length;
+
+        const ans = [];
+
+        // i <l<r
+        nums.sort((a, b) => a - b);
+        for (let i = 0; i < n - 2; ) {
+            if (nums[i] > 0) break;
+            const target = -nums[i];
+
+            let l = i + 1,
+                r = n - 1;
+            while (l < r) {
+                const sum = nums[l] + nums[r];
+                if (sum === target) {
+                    ans.push([nums[i], nums[l], nums[r]]);
+                    l += 1;
+                    while (nums[l] === nums[l - 1]) l += 1;
+                    r -= 1;
+                    while (nums[r] === nums[r + 1]) r -= 1;
+                } else if (sum < target) {
+                    l += 1;
+                    while (nums[l] === nums[l - 1]) l += 1;
+                } else {
+                    r -= 1;
+                    while (nums[r] === nums[r + 1]) r -= 1;
+                }
+            }
+
+            i+=1;
+            while(nums[i]===nums[i-1]) i+=1;
+        }
+
+        return ans;
+    }
+}
