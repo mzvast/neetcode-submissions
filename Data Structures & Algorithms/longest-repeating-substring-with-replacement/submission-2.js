67ba89b35cd7c748b0fa083e0b12c14300f4fad5
mzvast <mzvast@gmail.com>
2026-09-24 13:20:54 +0800

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        // len - maxFreq <=k
+        // ans = Max len
+
+        const n = s.length,
+            win = {};
+
+        let l = 0,
+            r = 0,
+            maxFreq = 0,
+            ans=0;
+
+        while (r < n) {
+            const toAdd = s[r++];
+            if (!win[toAdd]) win[toAdd] = 1;
+            else win[toAdd] += 1;
+
+            maxFreq = Math.max(maxFreq, win[toAdd]);
+            while (r - l - maxFreq > k) {
+                // shrink
+                const toDel = s[l++];
+                win[toDel] -= 1;
+            }
+
+            ans = Math.max(ans, r - l);
+        }
+
+        return ans;
+    }
+}
